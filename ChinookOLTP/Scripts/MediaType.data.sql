91ba05750a39b2cf1456ea05367b03f8f5fdf1dc
Your Name <you@example.com>
2025-05-04 03:25:01 -0400

    2script para insertar datos

---
+++ ChinookOLTP/Scripts/MediaType.data.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO [dbo].[MediaType] ([Name]) VALUES
+    (N'MPEG audio file'),
+    (N'Protected AAC audio file'),
+    (N'Protected MPEG-4 video file'),
+    (N'Purchased AAC audio file'),
+    (N'AAC audio file');
---
+++ ChinookOLTP/Scripts/Script.PostDeployment1.sql
@@ -0,0 +1,23 @@
+﻿/*
+Plantilla de script posterior a la implementación							
+--------------------------------------------------------------------------------------
+ Este archivo contiene instrucciones de SQL que se anexarán al script de compilación.		
+ Use la sintaxis de SQLCMD para incluir un archivo en el script posterior a la implementación.			
+ Ejemplo:      :r .\miArchivo.sql								
+ Use la sintaxis de SQLCMD para hacer referencia a una variable en el script posterior a la implementación.		
+ Ejemplo:      :setvar TableName miTabla							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+
+:r .\Genre.data.sql
+:r .\MediaType.data.sql
+:r .\Artist.data.sql
+:r .\Album.data.sql
+:r .\Track.data.sql
+:r .\Employee.data.sql
+:r .\Customer.data.sql
+:r .\Invoice.data.sql
+:r .\InvoiceLine.data.sql
+:r .\Playlist.data.sql
+:r .\PlaylistTrack.data.sql
